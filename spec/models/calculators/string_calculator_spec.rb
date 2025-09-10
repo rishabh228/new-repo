53f8275653ab1e350108e09bab0bbeccacf833c8
Rishabh Lodhi <rishabhlodhi9@gmail.com>
2025-09-10 15:30:00 +0000

    refactor the code

--- spec/models/calculators/string_calculator_spec.rb
+++ spec/models/calculators/string_calculator_spec.rb
@@ -2,43 +2,36 @@ require 'rails_helper'
 
 RSpec.describe Calculators::StringCalculator, type: :model do
   describe '#add' do
+   let (:calculator) { Calculators::StringCalculator.new} 
     it 'returns 0 for an empty string' do
-      calculator = Calculators::StringCalculator.new
       expect(calculator.add('')).to eq(0)
     end
 
     it 'returns the number itself for a single number' do
-      calculator = Calculators::StringCalculator.new
       expect(calculator.add('1')).to eq(1)
     end
 
     it 'returns the sum of two numbers' do
-      calculator = Calculators::StringCalculator.new
       expect(calculator.add('1,2')).to eq(3)
     end
 
     it 'returns the sum of multiple numbers' do
-      calculator = Calculators::StringCalculator.new
       expect(calculator.add('1,2,3')).to eq(6)
     end
 
     it 'handles new lines between numbers' do
-      calculator = Calculators::StringCalculator.new
       expect(calculator.add("1\n2,3")).to eq(6)
     end
 
     it 'supports different delimiters' do
-      calculator = Calculators::StringCalculator.new
       expect(calculator.add("//;\n1;2")).to eq(3)
     end
 
     it 'throws an exception for negative numbers' do
-      calculator = Calculators::StringCalculator.new
       expect { calculator.add('1,-2') }.to raise_error(Calculators::NegativeNumberError, 'negative numbers are not allowed: -2')
     end
 
     it 'throws an exception for multiple negative numbers' do
-      calculator = Calculators::StringCalculator.new
       expect { calculator.add('1,-2,-3') }.to raise_error(Calculators::NegativeNumberError, 'negative numbers are not allowed: -2, -3')
     end
   end
